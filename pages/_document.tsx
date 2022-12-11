76a4670bcab53e4ec5c91dfc1e83afbe28f628b3
Phoenix Zhong <phoenixson191@gmail.com>
2022-12-11 21:28:55 +0800

    fix: use local css instead of remote

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://unpkg.com/prismjs@0.0.1/themes/prism-okaidia.css"
-        />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
